cdc1db22cdcdc1d96fb3e9a854847c671b246e4e
Stijlbreuk Generic <generic@stijlbreuk.nl>
2022-08-03 12:20:03 +0430

    .

--- src/repositories/backend.repository.ts
+++ src/repositories/backend.repository.ts
@@ -2,6 +2,6 @@ import { ApolloClientRepository } from './apollo.repository';
 
 export class BackendRepository extends ApolloClientRepository {
   constructor() {
-    super(`${process.env.VUE_API_URL}/graphql`);
+    super(`${process.env.VUE_APP_API_URL}/graphql`);
   }
 }
