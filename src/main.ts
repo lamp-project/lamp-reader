21329431195bb4dcb8f64808042c09e517bb3027
Stijlbreuk Generic <generic@stijlbreuk.nl>
2022-08-11 13:37:42 +0430

    feat: PWA

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
 
 import { IonicVue } from '@ionic/vue';
@@ -25,10 +25,11 @@ import './theme/variables.css';
 /* Theme base */
 import './theme/base.css';
 
-const app = createApp(App)
-  .use(IonicVue)
-  .use(router);
-  
+// Added by the CLI
+import './registerServiceWorker';
+
+const app = createApp(App).use(IonicVue).use(router);
+
 router.isReady().then(() => {
   app.mount('#app');
 });
