8c81e372e9b0f4d4f54b8cad91680d0be01f2cd1
Stijlbreuk Generic <generic@stijlbreuk.nl>
2022-08-21 15:57:48 +0430

    .

--- src/components/utils/AsyncChild.vue
+++ src/components/utils/AsyncChild.vue
@@ -29,6 +29,7 @@ export default defineComponent({
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     onErrorCaptured((error) => {
       errorMessage.value = 'Something went wrong!';
+      throw error;
     });
     return {
       warningOutline,
